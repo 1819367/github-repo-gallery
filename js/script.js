fa22e68c02f0127d753993c9dbebc8ee843afc3b
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-04-03 12:33:45 -0700

    click event and functions to fetch and display specific repo info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const overviewDiv = document.querySelector(".overview"); //global variable to select div with class "overview". This is where personal profile info will appear.
 const username = 1819367; //global variable with my GitHub username
 const repoList = document.querySelector(".repo-list"); //global variable to select the unordered list to display the repos
+const infoSection = document.querySelector(".repos"); //selects section where all the repo info appears
+const repoData = document.querySelector(".repo-data"); //selects the section where the individual repo data will appear
+
 
 //async function to fetch info from personal GitHub profile
 const getProfile = async function () {
@@ -49,4 +52,49 @@ const displayRepos = function (repos) {
         li.innerHTML = `<h3>${repo.name}</h3>`; //an <h3> element with the repo name
         repoList.append(li); //append the li element to repoList unordered list element
     }
+};
+
+//add click event to the repoList on the unordered list,  pass it event (e) paramenter
+repoList.addEventListener("click", function (e) {
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText; //variable to target the innerText where the event happens
+        // console.log(repoName);//logs out 'clicked' event
+        fetchOneRepo(repoName); //calls the async function when the click event happens
+    }
+});
+
+//async function to get specific repo information that accepts repoName as a parameter
+const fetchOneRepo = async function (repoName) {
+    const soleRepo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await soleRepo.json(); //variable to resolve and save JSON response
+    console.log(repoInfo); //logs out fetched repo info in console
+
+    //create a languages array
+    const fetchLanguages = await fetch(repoInfo.languages_url);
+    const languageData = await fetchLanguages.json(); //variable to save the JSON response
+    console.log(languageData); //logs out the coding languages 
+
+    const languages = []; //empty array to add each language
+    for (let language in languageData) { //for ...in loop to loop over the object languageData
+        languages.push(language); //push the language to the end of the languages array
+    }
+    displaySoleRepo(repoInfo, languages); //call the function to display the selected repo info
+};
+
+//create a function to display specific repo info that accepts two parameters- repoInfo and languages
+const displaySoleRepo = function (repoInfo, languages) {
+    repoData.innerHTML = ""; //empty the HTML of the section with a class of "repo-data"
+    //create a new div element to add certain information about the selected repo to the innerHTML
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <h3>Name: ${repoInfo.name}</h3>
+        <p>Description: ${repoInfo.description}</p>
+        <p>Default Branch: ${repoInfo.default_branch}</p>
+        <p>Languages: ${languages.join(", ")}</p>
+        <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+        `;
+    repoData.append(newDiv); //append the new div element to the section with a class (repo-data)
+    repoData.classList.remove("hide"); //unhide the element with the "repo-data" class
+    infoSection.classList.add("hide"); //hide the element with the class of "repos"
+
 };
